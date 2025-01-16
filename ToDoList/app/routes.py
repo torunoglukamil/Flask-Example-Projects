8b2d4def208647ccaf82737ee231799af2fb5c03
Mehmet Kamil Torunoğlu <mehmet.torunoglu@turksat.com.tr>
2025-01-16 19:48:32 +0300

    todo list example

---
+++ ToDoList/app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+from app.models import Todo
+from app import db
+
+@app.before_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def index():
+	incomplete = Todo.query.filter_by(complete=False).all()
+	complete = Todo.query.filter_by(complete=True).all()
+
+	return render_template('index.html', incomplete=incomplete, complete=complete)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+	todo = Todo(text=request.form['todoitem'], complete=False)
+	db.session.add(todo)
+	db.session.commit()
+
+	return redirect(url_for('index'))
+
+
+@app.route('/complete/<id>')
+def complete(id):
+
+	todo = Todo.query.filter_by(id=int(id)).first()
+	todo.complete = True
+	db.session.commit()
+
+	return redirect(url_for('index'))
---
+++ ToDoList/wsgi_scripts/app.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, 'C:\\Users\\kamil\\Desktop\\FlaskProjects\\ToDoList')
+from app import app as application
